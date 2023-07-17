794bb7a6b2cd4e09358e8812d2fcbfdcd4011994
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-17 14:25:19 +0300

    Create Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
   <Header />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-vh-100"></div>
+  <footer id="footer" class="bg-white shadow-top">
+    <div class="container text-center py-4">
+      <h3 class="text-main">About</h3>
+      <p>
+        The project serves educational purposes and is not intended for practical use. The products featured herein do
+        not represent their actual value, description, or composition. The information provided is purely fictional and
+        should not be relied upon for any real-world applications.
+      </p>
+      <hr class="divider border-2">
+      <small>
+        Copyright &copy; 2022 - 2023
+        <a
+            href="https://github.com/FCTL3314/ShopTracker"
+            class="project-ref text-main-light"
+            target="_blank"
+        >
+          StoreTracker
+        </a>
+      </small>
+    </div>
+  </footer>
+</template>
+
+<style lang="sass" scoped>
+.project-ref
+  color: $color-main
+  text-decoration: none
+
+.project-ref:hover
+  color: $color-main-light
+  text-decoration: underline
+
+.shadow-top
+  box-shadow: 0 -0.125rem 1rem rgba(0, 0, 0, 0.075) !important
+</style>
