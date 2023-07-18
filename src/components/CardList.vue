b986c8c35f1f2d300d545b0b93461e818afcaf62
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-18 13:24:26 +0300

    Create Content and CardList components

---
+++ src/components/CardList.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="row justify-content-center">
+    <div class="text-center py-3">
+      <h2 class="text-main">
+        {{ title }}
+      </h2>
+      <p class="fs-4">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import '@/assets/sass/main'
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container-fluid position-relative">
+    <div class="container min-vh-100 bg-light">
+      <slot></slot>
+    </div>
+  </div>
+</template>
