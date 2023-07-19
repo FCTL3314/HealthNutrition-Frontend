a9959edfd5078784d438a2e3cab822e235b45917
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-19 14:42:42 +0300

    Create Pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,130 @@
+<script setup>
+import {computed} from 'vue';
+
+
+const props = defineProps({
+  totalPages: {
+    type: Number,
+    required: true,
+  },
+  currentPage: {
+    type: Number,
+    default: 1,
+  },
+  maxVisibleButtons: {
+    type: Number,
+    default: 9,
+  },
+});
+
+const startPage = computed(() => {
+  if (props.currentPage === 1) {
+    return 1;
+  }
+  return props.currentPage - 1;
+});
+
+const pages = computed(() => {
+  const range = [];
+  for (
+      let i = startPage.value;
+      i <= Math.min(startPage.value + props.maxVisibleButtons - 1, props.totalPages);
+      i++
+  ) {
+    range.push({
+      name: i,
+      isDisabled: i === props.currentPage,
+    });
+  }
+
+  return range;
+});
+
+const isInFirstPage = computed(() => props.currentPage === 1);
+const isInLastPage = computed(() => props.currentPage === props.totalPages);
+
+const emits = defineEmits(['pagechanged'])
+
+const onClickFirstPage = () => {
+  emits('pagechanged', 1);
+};
+
+const onClickPreviousPage = () => {
+  emits('pagechanged', props.currentPage - 1);
+};
+
+const onClickPage = (page) => {
+  emits('pagechanged', page);
+};
+
+const onClickNextPage = () => {
+  emits('pagechanged', props.currentPage + 1);
+};
+
+const onClickLastPage = () => {
+  emits('pagechanged', props.totalPages);
+};
+
+</script>
+
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <button
+            type="button"
+            class="page-link"
+            :class="{disabled: isInFirstPage}"
+            @click="onClickFirstPage"
+        >
+          <span aria-hidden="true">First</span>
+        </button>
+      </li>
+      <li class="page-item">
+        <button
+            type="button"
+            class="page-link"
+            :class="{disabled: isInFirstPage}"
+            @click="onClickPreviousPage"
+        >
+          <span aria-hidden="true">&laquo;</span>
+        </button>
+      </li>
+      <li v-for="page in pages" class="page-item">
+        <button
+            type="button"
+            class="page-link"
+            :class="{disabled: page.isDisabled}"
+            @click="onClickPage(page.name)"
+        >
+          {{ page.name }}
+        </button>
+      </li>
+      <li class="page-item">
+        <button
+            type="button"
+            class="page-link"
+            :class="{disabled: isInLastPage}"
+            @click="onClickNextPage"
+        >
+          <span aria-hidden="true">&raquo;</span>
+        </button>
+      </li>
+      <li class="page-item">
+        <button
+            type="button"
+            class="page-link"
+            :class="{disabled: isInLastPage}"
+            @click="onClickLastPage"
+        >
+          <span aria-hidden="true">Last</span>
+        </button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="sass" scoped>
+.page-link:focus
+  background-color: unset
+</style>
