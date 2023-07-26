ead7e4ede88cd5b4b732b164191e983c55b78fed
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-26 15:15:02 +0300

    Create auth-headers service

---
+++ src/services/auth-headers.js
@@ -0,0 +1,10 @@
+import store from '@/store/index'
+
+
+export default function () {
+  const accessToken = store.getters["auth/accessToken"];
+
+  return accessToken
+    ? {Authorization: `Bearer ${accessToken}`}
+    : {Authorization: ''};
+}
