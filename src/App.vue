99237fc6e922818a6ee6a5cd0e4bc9bb09eb1f42
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-26 15:16:24 +0300

    Recreate access token every "accessTokenLifeTime" milliseconds

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <script setup>
+import api from '@/api/index';
 import {RouterView} from 'vue-router';
+import {onBeforeMount, onMounted} from "vue";
+import {useStore} from 'vuex';
+import constants from '@/constants/index';
 import ContentWrapper from '@/components/ContentWrapper.vue';
 import HeaderSection from '@/components/HeaderSection.vue';
 import FooterSection from '@/components/FooterSection.vue';
+
+const store = useStore()
+
+async function getAccessToken() {
+  try {
+    const response = await api.users.refreshToken({
+      refresh: store.getters["auth/refreshToken"],
+    });
+    localStorage.setItem("accessToken", response.data.access)
+    store.commit('auth/setAccessToken', response.data.access)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onBeforeMount(() => {
+  store.commit('auth/initializeStore')
+})
+
+onMounted(async () => {
+  setInterval(async () => {
+    await getAccessToken()
+  }, constants.accessTokenLifeTime)
+})
 </script>
 
 <template>
