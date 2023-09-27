adbb7ada444fca2d51622c5f6df4cb51b38351eb
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-09-27 14:39:42 +0300

    Create ALLOWED_IMAGE_EXTENSIONS constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,10 @@ export const BASE_API_URL = 'http://127.0.0.1:8000/api/v1/'
 export const FRONTEND_REPOSITORY_URL = 'https://github.com/FCTL3314/StoreTracker-Frontend'
 export const BACKEND_REPOSITORY_URL = 'https://github.com/FCTL3314/StoreTracker-Backend'
 export const DJANGO_ADMIN_URL = 'http://127.0.0.1:8000/admin/'
+export const ALLOWED_IMAGE_EXTENSIONS = [
+  'image/png',
+  'image/jpeg',
+  'image/gif',
+  'image/webp',
+  'image/heic',
+].join(', ')
