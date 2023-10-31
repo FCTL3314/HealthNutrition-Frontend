f34d20bed224ab4a7222912d6a9fa3d60502536d
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-10-31 15:04:40 +0300

    Added replies toggle and reply buttons

--- src/components/comments/CommentBlock.vue
+++ src/components/comments/CommentBlock.vue
@@ -1,6 +1,9 @@
 <script setup>
 import {getUserImage} from '@/utils';
 import moment from 'moment';
+import CaretDownFillIcon from "@/components/icons/CaretDownFillIcon.vue";
+import {ref} from "vue";
+import CaretUpFillIcon from "@/components/icons/CaretUpFillIcon.vue";
 
 defineProps({
   text: {
@@ -15,12 +18,25 @@ defineProps({
     type: String,
     required: true,
   },
+  has_replies: {
+    type: Boolean,
+    required: true,
+  },
+  replies_count: {
+    type: Number,
+    required: true,
+  },
   edited: {
     type: Boolean,
     required: true,
   }
 })
 
+const isRepliesOpened = ref(false);
+
+function toggleReplies() {
+  isRepliesOpened.value = !isRepliesOpened.value;
+}
 </script>
 
 <template>
@@ -41,14 +57,37 @@ defineProps({
         {{ author.username }}
       </router-link>
       <span class="text-body-secondary">
-        <span>{{ moment(created_at).fromNow() }}</span>
-        <span class="ms-1" v-if="edited">(edited)</span>
-      </span>
-      <p class="text-break">{{ text }}</p>
+          <span>{{ moment(created_at).fromNow() }}</span>
+          <span class="ms-1" v-if="edited">(edited)</span>
+        </span>
+      <p class="text-break mb-0">{{ text }}</p>
+
+
+      <div class="d-flex align-items-center text-main-light">
+        <button
+            @click="has_replies && toggleReplies()"
+            class="btn btn-comment"
+        >
+          <template v-if="has_replies">
+            <caret-up-fill-icon v-if="isRepliesOpened"/>
+            <caret-down-fill-icon v-else/>
+          </template>
+          <span class="ms-1">
+            {{ replies_count }} Replies
+          </span>
+        </button>
+        <span class="mx-1">&middot;</span>
+        <button class="btn btn-comment">
+          Reply
+        </button>
+      </div>
+
+
     </div>
   </div>
 </template>
 
 <style scoped lang="sass">
+@import "@/assets/sass/main"
 @import "@/assets/sass/comments"
 </style>
