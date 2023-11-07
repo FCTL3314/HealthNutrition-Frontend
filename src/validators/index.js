349fcded3018da739f43b749ee8167e687385621
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-07 14:40:43 +0300

    Move vue props validation functions to the validators directory

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,29 +1,6 @@
-import {alphaNum, helpers, maxLength, minLength, required} from "@vuelidate/validators";
+import {COMMENTS_ALLOWED_CONTENT_TYPES} from "@/constants";
 
-export const alphaNumAndSpecialCharacters = helpers.regex(/^[a-z\d.@\-_+]*$/i);
 
-export const usernameValidCharacters = helpers.withMessage(
-    "Usernames may contain only alphanumeric and _ @ + . - characters",
-    alphaNumAndSpecialCharacters,
-);
-
-export const containsLetter = (value) => /[a-z]/i.test(value);
-
-export const passwordContainsLetter = helpers.withMessage(
-    "This password does not have to be completely numeric",
-    containsLetter,
-);
-
-export const usernameValidators = {
-    minLength: minLength(4),
-    maxLength: maxLength(32),
-    usernameValidCharacters: usernameValidCharacters,
-};
-
-export const passwordValidators = {
-    required,
-    alphaNum,
-    minLength: minLength(8),
-    maxLength: maxLength(32),
-    passwordContainsLetter: passwordContainsLetter,
-};
+export function isContentTypeAllowed(value) {
+    return COMMENTS_ALLOWED_CONTENT_TYPES.includes(value);
+}
---
+++ src/validators/vuelidate.js
@@ -0,0 +1,29 @@
+import {alphaNum, helpers, maxLength, minLength, required} from "@vuelidate/validators";
+
+export const alphaNumAndSpecialCharacters = helpers.regex(/^[a-z\d.@\-_+]*$/i);
+
+export const usernameValidCharacters = helpers.withMessage(
+    "Usernames may contain only alphanumeric and _ @ + . - characters",
+    alphaNumAndSpecialCharacters,
+);
+
+export const containsLetter = (value) => /[a-z]/i.test(value);
+
+export const passwordContainsLetter = helpers.withMessage(
+    "This password does not have to be completely numeric",
+    containsLetter,
+);
+
+export const usernameValidators = {
+    minLength: minLength(4),
+    maxLength: maxLength(32),
+    usernameValidCharacters: usernameValidCharacters,
+};
+
+export const passwordValidators = {
+    required,
+    alphaNum,
+    minLength: minLength(8),
+    maxLength: maxLength(32),
+    passwordContainsLetter: passwordContainsLetter,
+};
