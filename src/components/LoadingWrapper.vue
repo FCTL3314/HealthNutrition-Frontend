9b16dcb1a73a313c712cd68306d6e6bc2a2f0060
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-11-07 14:40:56 +0300

    Create LoadingWrapper.vue

---
+++ src/components/LoadingWrapper.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div v-if="isLoading" class="loading">
+    <div class="spinner-border spinner-border-lg" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+    <p class="fs-4 text-main-light">Loading</p>
+  </div>
+  <slot v-else></slot>
+</template>
+
+<style scoped lang="sass">
+@import "@/assets/sass/variables"
+
+
+.loading
+  text-align: center
+  padding: 6em 0 0 0
+  color: $color-main-light
+
+.spinner-border
+  &-lg
+    width: 3rem
+    height: 3rem
+</style>
