39d759a3a238bc0bae22beee30248ea7f430663f
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-04 13:15:15 +0300

    Added Christmas tiling image and event-background-christmas style

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,27 @@
 import {RouterView} from "vue-router";
 import HeaderSection from "@/components/HeaderSection.vue";
 import FooterSection from "@/components/FooterSection.vue";
+import {computed, onMounted} from "vue";
+
+
+const currentDate = new Date()
+const currentMonth = currentDate.getMonth() + 1
+
+const eventBackgroundClass = computed(() => {
+  const monthEventBackgroundClassMap = {
+    "12": "event-background-christmas",
+    "1": "event-background-christmas",
+  };
+  return monthEventBackgroundClassMap[currentMonth];
+})
+
+onMounted(() => {
+  document.body.classList.add(eventBackgroundClass.value);
+})
 </script>
 
 <template>
-  <div class="headed-indentation">
+  <div class="header-indentation">
     <header-section/>
   </div>
   <div class="container position-relative">
@@ -24,7 +41,13 @@ body
   font-family: 'Quicksand', 'sans-serif' !important
   background-color: $color-pink-light !important
 
-.headed-indentation
+.event-background
+  background-repeat: repeat !important
+
+  &-christmas
+    background-image: url("@/assets/images/christmas-tiling.png") !important
+
+.header-indentation
   margin: 0.75rem 0.75rem $between-components-indentation 0.75rem !important
 
 .footer-indentation
