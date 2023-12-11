f3f9086e3e0f3fe4f93a3884f15f8e9ece4c07f6
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-11 13:02:08 +0300

    Created ComparisonGroupGreeting.vue component

---
+++ src/components/greetings/ComparisonGroupGreeting.vue
@@ -0,0 +1,23 @@
+<script setup>
+import BaseGreeting from "@/components/greetings/BaseGreeting.vue";
+
+
+defineProps({
+  comparisonGroupName: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <base-greeting
+      :title="comparisonGroupName"
+      description="The statistics for this comparison group are displayed here, as well as average indicators for the
+      products of this comparison group. Products that stand out e.g. the most high-calorie product, marked with tags."
+  />
+</template>
+
+<style scoped lang="sass">
+@import '@/assets/sass/main'
+</style>
