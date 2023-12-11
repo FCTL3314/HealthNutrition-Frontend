63dbfa00e87d5efc9294aaa8fc2239ebc0f48676
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-11 13:02:23 +0300

    Created TheTag.vue component

---
+++ src/components/TheTag.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  bgColorClass: {
+    type: String,
+    default: "bg-primary",
+  },
+  textColorClass : {
+    type: String,
+    default: "text-white",
+  },
+})
+</script>
+
+<template>
+  <div class="tag" :class="[bgColorClass, textColorClass]">
+    <span class="fw-semibold">{{ text }}</span>
+  </div>
+</template>
+
+<style scoped lang="sass">
+@import "@/assets/sass/main"
+
+
+.tag
+  width: max-content
+  border-radius: $component-rounding
+  padding: .3em .7em
+</style>
