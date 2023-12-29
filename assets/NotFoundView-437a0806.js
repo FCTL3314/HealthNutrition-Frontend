a48c6f07a4512d20a92b7a65bc4b31ca6695db85
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-29 15:08:58 +0300

    Updates

--- assets/NotFoundView-437a0806.js
+++
@@ -1,2 +0,0 @@
-import{E as e}from"./ErrorSection-9b40fde7.js";import{o as t,a as o}from"./index-09159599.js";const c={__name:"NotFoundView",setup(r){return(a,n)=>(t(),o(e,{title:"404 Not Found",description:`We were unable to find the page you were interested in,\r
-                   it seems that it was deleted or did not exist at all.`}))}};export{c as default};
