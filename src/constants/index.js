38af505087487600df682a39cf76f69c172e3881
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-12-29 12:06:21 +0300

    Change env variables implementation

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 export const TITLE_ENDING = "Health Nutrition";
-export const BASE_BACKEND_URL = import.meta.env.VITE_BASE_BACKEND_URL;
-export const BASE_BACKEND_API_URL = import.meta.env.VITE_BASE_BACKEND_API_URL;
-export const BACKEND_ADMIN_URL = import.meta.env.VITE_BACKEND_ADMIN_URL;
+export const BASE_BACKEND_URL = import.meta.env.BASE_BACKEND_URL || "http://127.0.0.1:8000/";
+export const BASE_BACKEND_API_URL = import.meta.env.BASE_BACKEND_API_URL || "http://127.0.0.1:8000/api/v1/";
+export const BACKEND_ADMIN_URL = import.meta.env.BACKEND_ADMIN_URL || "http://127.0.0.1:8000/admin/";
 export const FRONTEND_REPOSITORY_URL = "https://github.com/FCTL3314/HealthNutrition-Frontend";
 export const BACKEND_REPOSITORY_URL = "https://github.com/FCTL3314/HealthNutrition-Backend";
 
